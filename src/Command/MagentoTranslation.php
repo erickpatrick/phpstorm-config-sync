cabf638c2d39021f3e3565df59ff36e302458e73
Erick Alvarenga <Erick.Alvarenga@nintendo.de>
2018-01-29 11:03:01 +0100

    creates new command for to handle Magento translations

--- src/Command/MagentoTranslation.php
+++ src/Command/MagentoTranslation.php
@@ -1,5 +1,6 @@
 <?php namespace Nintendo\Translator\Command;
 
+use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -8,24 +9,37 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class MagentoTranslation extends Command
 {
+    /** @var \Nintendo\Translator\Data $transporter */
     private $transporter;
+
+    /** @var \Nintendo\Translator\Magento\Builder $builder */
     private $builder;
+
+    /** @var \Nintendo\Translator\Magento\Formatter $formatter */
     private $formatter;
+
+    /** @var \Nintendo\Translator\Magento\Merger $merger */
     private $merger;
+
+    /** @var \Nintendo\Translator\Magento\Creator $creator */
     private $creator;
 
     /**
      * MagentoTranslation constructor.
      * @param ContainerBuilder $container
-     * @throws \Exception
+     * @throws Exception
      */
     public function __construct(ContainerBuilder $container)
     {
-        $this->transporter = $container->get('transporter');
-        $this->builder = $container->get('builder');
-        $this->formatter = $container->get('formatter');
-        $this->merger = $container->get('merger');
-        $this->creator = $container->get('creator');
+        try {
+            $this->transporter = $container->get('transporter');
+            $this->builder = $container->get('builder');
+            $this->formatter = $container->get('formatter');
+            $this->merger = $container->get('merger');
+            $this->creator = $container->get('creator');
+        } catch (Exception $exception) {
+            echo "The service you're trying to get does not exist or it was not added to the container";
+        }
 
         parent::__construct();
     }
