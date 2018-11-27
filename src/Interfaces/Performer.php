4bfe58c3301f1ff2d5b8c45bac8841723f19c792
Erick Alvarenga <erick.alvarenga@nintendo.de>
2018-11-27 08:57:45 +0000

    removes unnecessary files from this repository

--- src/Interfaces/Performer.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Nintendo\Translator\Interfaces;
-
-interface Performer
-{
-    public function setNext(DataTransformer $next): void;
-    public function next(DataTransporter $data, $newData): DataTransporter;
-}
